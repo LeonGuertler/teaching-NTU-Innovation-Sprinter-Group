6f4b689fb490168714639272f05c5de4d374b71e
LeonGur <45076562+LeonGur@users.noreply.github.com>
2021-02-07 23:04:18 +0800

    comments (ugh)

--- DL code/Cartpole/algorithmic_solution_cartpole.py
+++ DL code/Cartpole/algorithmic_solution_cartpole.py
@@ -1,10 +1,17 @@
 import gym, time
+
+# create the environemt
 env = gym.make("CartPole-v1")
 
-for _ in range(15):
+for game_nr in range(15):
+    # initialize and reset game relevant variables
     score = action = frame = done = 0
     observation = env.reset()
+
+    # play until solved or failed
     while not done:
+
+        # the following if statements determine the action and are based on intuition
         if observation[3]>.09:
             action = 1
         if observation[3]<-.09:
@@ -14,8 +21,11 @@ for _ in range(15):
                 action=1
             elif observation[1]<-.45:
                 action=0
+
+        # execute the choosen action in the enviornment
         observation, reward, done, info = env.step(action)
         env.render()
         score += reward
         frame += 1
-        print(score, "\t", observation[1])
+
+    print(f"Game_nr: {game_nr}\t\tScore: {score}", end="\r")
